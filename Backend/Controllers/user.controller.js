1984bd0db2e3e7eaf8b6eb117c272e7308993b38
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-06-26 14:52:32 +0545

    login of user with creation of jsonwebtoken.

--- Backend/Controllers/user.controller.js
+++ Backend/Controllers/user.controller.js
@@ -1,10 +1,11 @@
 import userModel from "../Models/user.model";
 import validator from "validator";
 import bcrypt from "bcrypt";
+import { generateToken } from "../utility/generate.token";
 
 export const registerUser = async (req, res, next) => {
   try {
-    const { email, fullName, password,role} = req.body;
+    const { email, fullName, password, role } = req.body;
 
     const EmailExists = await userModel.findOne({ email });
 
@@ -26,7 +27,7 @@ export const registerUser = async (req, res, next) => {
 
     const user = new userModel({
       email,
-      username:fullName,
+      username: fullName,
       password: hashedPassword,
       role,
     });
@@ -44,3 +45,36 @@ export const registerUser = async (req, res, next) => {
   }
 };
 
+export const loginUser = async (req, res, next) => {
+  try {
+    const { email, password, role } = req.body;
+
+    if (!email || !password || !role)
+      return next({ statusCode: 400, message: " Fields cannot be empty" });
+
+    const user = await userModel.findOne({ email });
+
+    // if no user exists -> null
+    if (!user)
+      return next({ statusCode: 400, message: "Incorrect email or password" });
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    // if password donot match -> false
+    if (!isPasswordMatch)
+      return next({ statusCode: 400, message: "Invalid Credentials" });
+
+    if (role !== user.role)
+      return next({
+        statusCode: 400,
+        message: "Account doesnot exist with this role.",
+      });
+
+    const token = generateToken(user);
+
+    return res
+      .status(200)
+      .json({ error: false, message: "Login Success", token });
+  } catch (error) {
+    console.log("Error in logging user", error);
+    return next({ statusCode: 500, message: "Failed to login." });
+  }
+};
